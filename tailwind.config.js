8a2e03029746a559e848f0c2f5b0856968c25a86
Radha743 <worldofnara777@gmail.com>
2023-02-06 23:53:09 +0530

    section 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
 	content: ["./*.html"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "375px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			fontFamily: {
+				publicSans: ["Public Sans", "sans-serif"],
+			},
+			colors: {
+				primaryDarkBlue: "hsl(233, 26%, 24%)",
+				primaryLimeGreen: "hsl(136, 65%, 51%)",
+				primaryBrightCyan: "hsl(192, 70%, 51%)",
+				grayishBlue: "hsl(233, 8%, 62%)",
+				lightGrayishBlue: "hsl(220, 16%, 96%)",
+				veryLightGray: "hsl(0, 0%, 90%)",
+				white: "hsl(0, 0%, 100%)",
+			},
+			backgroundImage: {
+				"mobile-intro": "url('/assets/images/bg-into-mobile.svg')",
+			},
+		},
 	},
 	plugins: [],
 };
